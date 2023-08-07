baa31c19870e6e7c0f2a129648673b111cbd89ce
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-08-07 15:08:40 +0200

    6

--- src/analyze.cu
+++ src/analyze.cu
@@ -1,26 +1,7 @@
 #include <cstdio>
-#include <climits>
+#include "cudaDeviceProperties.cuh"
 
-int main(int argCount, char *argVariables[]) {
-    for (int i = 1; i < argCount; i++) {
-        char *arg = argVariables[i];
-        if (strcmp(arg, "-adv") == 0) {
-            printf("The final file will provide advanced GPU information.\n");
-        } else if (strcmp(arg, "-fas") == 0) {
-            printf("The benchmarks will be executed faster than normal.\n");
-        } else {
-            printf("No commands were given.\n");
-        }
-    }
-    int coreSwitch = 0;
-    int deviceID = 0;
-    int coreQuerySize = 1024;
-    char cudaCoreQueryPath[coreQuerySize];
-    graphicCardInformation cardInformation = getDeviceProperties(cudaCoreQueryPath, coreSwitch, deviceID);
-    createOutputFile(cardInformation);
-}
-
-void createOutputFile(graphicCardInformation cardInformation) {
+void createOutputFile(CudaDeviceInfo cardInformation) {
     printf("Create the output file...\n");
     char output[] = "Output";
     FILE *csv = fopen(output, "w");
@@ -37,5 +18,24 @@ void createOutputFile(graphicCardInformation cardInformation) {
     fprintf(csv, "Registers_per_thread_block; %d; \"32-bit registers\"; ", cardInformation.registersPerThreadBlock);
     fprintf(csv, "Registers_per_SM; %d; \"32-bit registers\"; ", cardInformation.registersPerSM);
     fclose(csv);
+}
+
+int main(int argCount, char *argVariables[]) {
+    for (int i = 1; i < argCount; i++) {
+        char *arg = argVariables[i];
+        if (strcmp(arg, "-adv") == 0) {
+            printf("The final file will provide advanced GPU information.\n");
+        } else if (strcmp(arg, "-fas") == 0) {
+            printf("The benchmarks will be executed faster than normal.\n");
+        } else {
+            printf("No commands were given.\n");
+        }
+    }
+    int coreSwitch = 0;
+    int deviceID = 0;
+    int coreQuerySize = 1024;
+    char cudaCoreQueryPath[coreQuerySize];
+    CudaDeviceInfo cardInformation = getDeviceProperties(cudaCoreQueryPath, coreSwitch, deviceID);
+    createOutputFile(cardInformation);
 }
 
