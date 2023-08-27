2b2f3d5373ca68081fb6af9385487b20a04547fa
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-08-27 16:25:53 +0200

    next

--- src/Analyze_Random_Cores.cuh
+++ src/Analyze_Random_Cores.cuh
@@ -11,7 +11,9 @@ typedef struct BenchmarkThread {
     long long end;
 } BenchmarkThread;
 
-BenchmarkThread thread[65536];
+typedef struct Benchmark {
+    BenchmarkThread thread[65536];
+} Benchmark;
 
 static __device__ __inline__ int getLaneId() {
     int laneId;
@@ -37,42 +39,46 @@ static __device__ __inline__ long long getCounter() {
     return counter;
 }
 
-__global__ void simpleAdd(int n) {
+__global__ void simpleAdd(int n, Benchmark *host) {
     int current = (blockIdx.x * blockDim.x) + threadIdx.x;
-    thread[current].threadId = threadIdx.x;
-    thread[current].blockId = blockIdx.x;
-    thread[current].laneId = getLaneId();
-    thread[current].warpId = getWarpId();
-    thread[current].smId = getSmId();
-    int x[n];
-    int y[n];
-    int z[n];
+    host.thread[current].threadId = threadIdx.x;
+    host.thread[current].blockId = blockIdx.x;
+    host.thread[current].laneId = getLaneId();
+    host.thread[current].warpId = getWarpId();
+    host.thread[current].smId = getSmId();
+    int x[];
+    int y[];
+    int z[];
     for (int i = 0; i < n; i++) {
         x[i] = i;
         y[i] = (n-i)-1;
     }
-    thread[current].begin = getCounter();
+    host.thread[current].begin = getCounter();
     for (int i = 0; i < n; i ++) {
         z[i] = x[i] + y[i];
     }
-    thread[current].end = getCounter();
+    host.thread[current].end = getCounter();
 }
 
 void performRandomCoreBenchmark() {
-    simpleAdd<<<256, 256>>>(256);
+    Benchmark* benchmark;
+    cudaMallocManaged(&benchmark, 13631488);
+    simpleAdd<<<256, 256>>>(256, benchmark);
+    cudaDeviceSynchronize();
     char output[] = "Benchmark.csv";
     FILE *csv = fopen(output, "w");
     fprintf(csv, "threadId ; blockId ; laneId ; warpId ; smId ; begin ; end \n");
     for (int i = 0; i < 65536; i++) {
-        fprintf(csv, "%d ; ", thread[i].threadId);
-        fprintf(csv, "%d ; ", thread[i].blockId);
-        fprintf(csv, "%d ; ", thread[i].laneId);
-        fprintf(csv, "%d ; ", thread[i].warpId);
-        fprintf(csv, "%d ; ", thread[i].smId);
-        fprintf(csv, "%lld ; ", thread[i].begin);
-        fprintf(csv, "%lld \n", thread[i].end);
+        fprintf(csv, "%d ; ", benchmark.thread[i].threadId);
+        fprintf(csv, "%d ; ", benchmark.thread[i].blockId);
+        fprintf(csv, "%d ; ", benchmark.thread[i].laneId);
+        fprintf(csv, "%d ; ", benchmark.thread[i].warpId);
+        fprintf(csv, "%d ; ", benchmark.thread[i].smId);
+        fprintf(csv, "%lld ; ", benchmark.thread[i].begin);
+        fprintf(csv, "%lld \n", benchmark.thread[i].end);
     }
     fclose(csv);
+    cudaFree(benchmark);
 }
 
 #endif //GCPVE_C_C_ANALYZE_RANDOM_CORES_CUH
