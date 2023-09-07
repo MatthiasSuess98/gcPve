067a47a7ab05b69ae83473a79fca338e3ef04dd7
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-07 08:25:44 +0200

    next

--- evaluation.py
+++ evaluation.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     data = np.loadtxt("Benchmark_16bit.csv", delimiter=";", dtype="float")
     smId = data[:, 0]
-    time = data[:, 4]
+    time = data[:, 1]
     fig = plt.figure()
     plt.step(smId, time, color='green')
-    plt.title("Benchmark: Average computation time of a simple add statement.")
-    plt.xlabel("streaming multiprocessor (sm)")
+    plt.title("Benchmark: Average computation time of a simple add statement on sm 0.")
+    plt.xlabel("thread")
     plt.ylabel("computation time in ns")
     fig.savefig('Benchmark_16bit.pdf', bbox_inches='tight')
     quit()
