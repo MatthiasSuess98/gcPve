b414862057d6f4bf535639972fd557e0dcca7fe6
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-21 16:44:08 +0200

    next

--- src/10-Perform_Benchmark.cuh
+++ src/10-Perform_Benchmark.cuh
@@ -36,7 +36,7 @@ void performBenchmark1(GpuInformation info, BenchmarkProperties prop, InfoPropDe
 
     // Declare and initialize all core characteristics.
     std::vector<CoreCharacteristics> gpuCores;
-    CoreCharacteristics = CoreCharacteristics(0, 0, 0);
+    CoreCharacteristics gpuCore = CoreCharacteristics(0, 0, 0);
     for (int i = 0; i < info.multiProcessorCount; i++) {
         for (int j = 0; j < derivatives.hardwareWarpsPerSm; j++) {
             for (int k = 0; k < info.warpSize; k++) {
