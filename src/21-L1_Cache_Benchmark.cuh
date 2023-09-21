db9e96a385a0b22362db495decaa2645005414e5
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-21 18:07:44 +0200

    next

--- src/21-L1_Cache_Benchmark.cuh
+++ src/21-L1_Cache_Benchmark.cuh
@@ -33,11 +33,11 @@ __global__ void smallL1Benchmark(SmallDataCollection *ptr, GpuInformation info, 
     for (int preparationLoop = 0; preparationLoop < prop.numberOfTrialsBenchmark; preparationLoop++) {
         asm volatile ("ld.global.ca.u32 %0, [%1];" : "=r"(zero) : "l"(load) : "memory");
     }
-    asm volatile("mov.u64 %0, %%globaltimer;" : "=l"(start_time));
+    asm volatile("mov.u64 %0, %globaltimer;" : "=l"(start_time));
     for (int measureLoop = 0; measureLoop < prop.numberOfTrialsBenchmark; measureLoop++) {
         asm volatile ("ld.global.ca.u32 %0, [%1];" : "=r"(zero) : "l"(load) : "memory");
     }
-    asm volatile("mov.u64 %0, %%globaltimer;" : "=l"(end_time));
+    asm volatile("mov.u64 %0, %globaltimer;" : "=l"(end_time));
     unsigned int diff = (unsigned int) (end_time - start_time);
     (*ptr).time[pos] = (float) (diff / prop.numberOfTrialsBenchmark);
     printf("%d\n", diff);
