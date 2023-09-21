3841223ae194d780c9d481543df6d7eeb3202f70
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-21 19:32:02 +0200

    next

--- src/20-L1_Cache_Launcher.cuh
+++ src/20-L1_Cache_Launcher.cuh
@@ -51,9 +51,9 @@ SmallDataCollection performSmallL1Benchmark(GpuInformation info, BenchmarkProper
             }
             launchSmallL1Benchmarks(ptr, info, prop, derivatives);
             for (int blockLoop = 0; moveOn && (blockLoop < derivatives.smallNumberOfBlocks); blockLoop++) {
-                //printf("test");
+                printf("%d", (*ptr).mulp[blockLoop * info.warpSize]);
                 if (moveOn && ((*ptr).mulp[blockLoop * info.warpSize] == mulpLoop) && ((*ptr).time[blockLoop * info.warpSize] != 0)) {
-                    printf("test");
+                    //printf("test");
                     for (int freeLoop = 0; moveOn && (freeLoop < derivatives.smallNumberOfBlocksPerMulp); freeLoop++) {
                         if (moveOn && (finalCollection.time[(mulpLoop * derivatives.smallNumberOfBlocksPerMulp) + freeLoop] != 0)) {
                             for (int laneLoop = 0; moveOn && (laneLoop < info.warpSize); laneLoop++) {
