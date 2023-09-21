7cc861b90840fa5990060f7e980f4373db3f2704
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-21 19:39:44 +0200

    next

--- src/20-L1_Cache_Launcher.cuh
+++ src/20-L1_Cache_Launcher.cuh
@@ -51,11 +51,10 @@ SmallDataCollection performSmallL1Benchmark(GpuInformation info, BenchmarkProper
             }
             launchSmallL1Benchmarks(ptr, info, prop, derivatives);
             for (int blockLoop = 0; moveOn && (blockLoop < derivatives.smallNumberOfBlocks); blockLoop++) {
-                //printf("%d", (*ptr).mulp[blockLoop * info.warpSize]);
                 if (moveOn && ((*ptr).mulp[blockLoop * info.warpSize] == mulpLoop) && ((*ptr).time[blockLoop * info.warpSize] != 0)) {
-                    printf("test");
                     for (int freeLoop = 0; moveOn && (freeLoop < derivatives.smallNumberOfBlocksPerMulp); freeLoop++) {
                         if (moveOn && (finalCollection.time[(mulpLoop * derivatives.smallNumberOfBlocksPerMulp) + freeLoop] != 0)) {
+                            printf("test");
                             for (int laneLoop = 0; moveOn && (laneLoop < info.warpSize); laneLoop++) {
                                 finalCollection.mulp[(((mulpLoop * derivatives.smallNumberOfBlocksPerMulp) + freeLoop) * info.warpSize) + laneLoop] = (*ptr).mulp[(blockLoop * info.warpSize) + laneLoop];
                                 finalCollection.warp[(((mulpLoop * derivatives.smallNumberOfBlocksPerMulp) + freeLoop) * info.warpSize) + laneLoop] = (*ptr).warp[(blockLoop * info.warpSize) + laneLoop];
