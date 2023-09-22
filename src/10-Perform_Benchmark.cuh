ea85a25f628ce87ca18c0d6ff14679961b106ca0
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-22 08:41:51 +0200

    next

--- src/10-Perform_Benchmark.cuh
+++ src/10-Perform_Benchmark.cuh
@@ -140,11 +140,12 @@ void performBenchmark1(GpuInformation info, BenchmarkProperties prop, InfoPropDe
                     }
                     if (dontFits[bestHardwareWarp] > prop.maxDontFit) {
                         printf("%d", dontFits[bestHardwareWarp]);
+                        printf("test1");
                         for (int laneLoop = 0; laneLoop < info.warpSize; laneLoop++) {
                             gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (hardwareWarpScore * info.warpSize) + laneLoop].setTypicalL1Time(data.time[blockLoop + laneLoop]);
                         }
                     } else {
-                        printf("test");
+                        printf("test2");
                         for (int laneLoop = 0; laneLoop < info.warpSize; laneLoop++) {
                             currentTime = gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (bestHardwareWarp * info.warpSize) + laneLoop].getTypicalL1Time();
                             gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (bestHardwareWarp * info.warpSize) + laneLoop].setTypicalL1Time((data.time[blockLoop + laneLoop] + currentTime) / 2);
