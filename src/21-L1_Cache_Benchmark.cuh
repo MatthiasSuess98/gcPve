c7270447fcfc7ebb4aa951faed9df55224555362
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-22 20:20:25 +0200

    next

--- src/21-L1_Cache_Benchmark.cuh
+++ src/21-L1_Cache_Benchmark.cuh
@@ -23,8 +23,8 @@ __global__ void smallL1Benchmark(SmallDataCollection *ptr, int requiredLane, uns
     asm volatile ("mov.u32 %0, %%smid;" : "=r"(mulp));
     asm volatile ("mov.u32 %0, %%warpid;" : "=r"(warp));
     asm volatile ("mov.u32 %0, %%laneid;" : "=r"(lane));
-    if (lane == requiredLane && ((warp == 0) || (warp == 1) || (warp == 2) || (warp == 3))) {
-        printf("test");
+    printf("%d", lane);
+    if ((lane == requiredLane) && ((warp == 0) || (warp == 1) || (warp == 2) || (warp == 3))) {
         (*ptr).mulp[pos] = mulp;
         (*ptr).warp[pos] = warp;
         (*ptr).lane[pos] = lane;
