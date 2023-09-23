4050ea9a70375f0072b7c5c8c26f9e1ab01b13b4
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-23 20:40:05 +0200

    next

--- src/05-Data_Collection.cuh
+++ src/05-Data_Collection.cuh
@@ -4,24 +4,45 @@
 #include <vector>
 
 /**
- * Data structure for data collections.
+ * Data structure for small data collections.
  */
-typedef struct DataCollection {
-    std::vector<int> mulp;
-    std::vector<int> warp;
-    std::vector<int> lane;
-    std::vector<long long int> time;
-} DataCollection;
+typedef struct SmallDataCollection {
+    // small = 16 bit
+    int mulp[65536];
+    int warp[65536];
+    int lane[65536];
+    float time[65536];
+} SmallDataCollection;
+
 
 /**
- * Data structure for data collections.
+ * Data structure for medium data collections.
  */
-__device__ typedef struct DeviceCollection {
+typedef struct MediumDataCollection {
+    // medium = 24 bit
+    int mulp[16777216];
+    int warp[16777216];
+    int lane[16777216];
+    float time[16777216];
+} MediumDataCollection;
+
+
+/**
+ * Data structure for large data collections.
+ */
+typedef struct LargeDataCollection {
+    // large = 32 bit
+    int mulp[4294967296];
+    int warp[4294967296];
+    int lane[4294967296];
+    float time[4294967296];
+} LargeDataCollection;
+
+
+typedef struct collection {
     std::vector<int> mulp;
-    std::vector<int> warp;
-    std::vector<int> lane;
-    std::vector<long long int> time;
-} DeviceCollection;
+} collection;
+
 
 #endif //GCPVE_04_DATA_COLLECTION_CUH
 
