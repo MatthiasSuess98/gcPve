04c0f58891c399e1321dd5e24bd420d377c96359
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-23 20:20:21 +0200

    next

--- src/05-Data_Collection.cuh
+++ src/05-Data_Collection.cuh
@@ -13,6 +13,15 @@ typedef struct DataCollection {
     std::vector<long long int> time;
 } DataCollection;
 
+/**
+ * Data structure for data collections.
+ */
+__device__ typedef struct DataCollection {
+    std::vector<int> mulp;
+    std::vector<int> warp;
+    std::vector<int> lane;
+    std::vector<long long int> time;
+} DataCollection;
 
 #endif //GCPVE_04_DATA_COLLECTION_CUH
 
