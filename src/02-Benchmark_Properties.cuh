bc47f667cae4a5768828df53ffcb394603bf32d9
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-24 18:33:02 +0200

    next

--- src/02-Benchmark_Properties.cuh
+++ src/02-Benchmark_Properties.cuh
@@ -32,8 +32,8 @@ BenchmarkProperties getBenchmarkProperties() {
     prop.numberOfTrialsPerform = 10;
     prop.numberOfTrialsLaunch = 10;
     prop.memoryOverlap = 2;
-    prop.maxDelta = 5.0;
-    prop.maxDontFit = 36 / ((int) prop.maxDelta);
+    prop.maxDelta = 6.0;
+    prop.maxDontFit = 32 / ((int) prop.maxDelta);
     // Size of the data collections.
     // Warning: If these three variables get updated, update the variables in 04-Core_Characteristics and in the kernels also!
     prop.small = 65536;
