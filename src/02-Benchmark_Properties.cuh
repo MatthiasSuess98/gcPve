45d4d9f9e835fd76c4a6449b466458a127db1d13
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-24 09:44:24 +0200

    next

--- src/02-Benchmark_Properties.cuh
+++ src/02-Benchmark_Properties.cuh
@@ -32,7 +32,7 @@ BenchmarkProperties getBenchmarkProperties() {
     // Initialize the properties and writes them into the final data structure.
     prop.numberOfTrialsPerform = 1;
     prop.numberOfTrialsLaunch = 10;
-    prop.numberOfTrialsDivisor = 64;
+    prop.numberOfTrialsDivisor = 1024;
     prop.memoryOverlap = 2;
     prop.maxDelta = 0;
     prop.maxDontFit = 3;
