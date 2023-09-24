c5e7f43bd75888cad27cae3365fd4230fddc20fc
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-24 20:02:06 +0200

    next

--- src/10-Perform_Benchmark.cuh
+++ src/10-Perform_Benchmark.cuh
@@ -189,7 +189,7 @@ void performSmallBenchmark(GpuInformation info, BenchmarkProperties prop, InfoPr
         data = performSmallL1Benchmark(info, prop, derivatives);
         for (int blockLoop = 0; blockLoop < prop.small; blockLoop = blockLoop + info.warpSize) {
             if (data.time[blockLoop] != 0) {
-                if (gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (data.warp[blockLoop] * info.warpSize) + data.lane[blockLoop]].getTypicalL1Time() == 0) {
+                if (gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (data.warp[blockLoop] * info.warpSize)].getTypicalL1Time() == 0.0) {
                     for (int laneLoop = 0; laneLoop < info.warpSize; laneLoop++) {
                         gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (data.warp[blockLoop] * info.warpSize) + laneLoop].setTypicalL1Time(((long double) data.time[blockLoop + laneLoop]) / ((long double) (derivatives.smallNumberOfTrialsDivisor * derivatives.smallNumberOfTrialsDivisor)));
                     }
