38ccc8f450285eb5fee12a2c2c7d331dbd1f5009
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-25 17:39:12 +0200

    next

--- src/10-Perform_Benchmark.cuh
+++ src/10-Perform_Benchmark.cuh
@@ -141,7 +141,9 @@ void performSmallBenchmark(GpuInformation info, BenchmarkProperties prop, InfoPr
         for (int j = 0; j < derivatives.hardwareWarpsPerSm; j++) {
             for (int k = 0; k < info.warpSize; k++) {
                 fprintf(csv, "%Lf", gpuCores[(i * derivatives.hardwareWarpsPerSm * info.warpSize) + (j * info.warpSize) + k].getTypicalL1Time());
-                fprintf(csv, ";");
+                if (k < (info.warpSize - 1)) {
+                    fprintf(csv, ";");
+                }
             }
             fprintf(csv, "\n");
         }
