bdae5a79b3d6891ea8f5f5defb7a0554709b96b9
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-25 20:27:27 +0200

    next

--- src/10-Perform_Benchmark.cuh
+++ src/10-Perform_Benchmark.cuh
@@ -54,81 +54,9 @@ void performSmallBenchmark(GpuInformation info, BenchmarkProperties prop, InfoPr
         data = performSmallL1Benchmark(info, prop, derivatives);
         for (int blockLoop = 0; blockLoop < prop.small; blockLoop = blockLoop + info.warpSize) {
             if (data.time[blockLoop] != 0) {
-                hardwareWarpScore = 0;
-                for (int hardwareWarpLoop = 0; hardwareWarpLoop < derivatives.hardwareWarpsPerSm; hardwareWarpLoop++) {
-                    dontFits[hardwareWarpLoop] = 0;
-                }
-                for (int hardwareWarpLoop = 0; hardwareWarpLoop < derivatives.hardwareWarpsPerSm; hardwareWarpLoop++) {
-                    if (gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (hardwareWarpLoop * info.warpSize)].getTypicalL1Time() != 0.0) {
-                        hardwareWarpScore++;
-                    }
-                }
-                if (hardwareWarpScore == 0) {
-                    for (int laneLoop = 0; laneLoop < info.warpSize; laneLoop++) {
-                        gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + laneLoop].setTypicalL1Time(((long double) data.time[blockLoop + laneLoop]) / ((long double) (derivatives.smallNumberOfTrialsDivisor * derivatives.smallNumberOfTrialsDivisor)));
-                    }
-                } else if (hardwareWarpScore == derivatives.hardwareWarpsPerSm) {
-                    for (int hardwareWarpLoop = 0; hardwareWarpLoop < derivatives.hardwareWarpsPerSm; hardwareWarpLoop++) {
-                        for (int laneLoop = 0; laneLoop < info.warpSize; laneLoop++) {
-                            if (std::abs(gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (hardwareWarpLoop * info.warpSize) + laneLoop].getTypicalL1Time() - (((long double) data.time[blockLoop]) / ((long double) (derivatives.smallNumberOfTrialsDivisor * derivatives.smallNumberOfTrialsDivisor)))) >= prop.maxDelta) {
-                                dontFits[hardwareWarpLoop]++;
-                            }
-                        }
-                    }
-                    smallestNumber = 0;
-                    bestHardwareWarp = 0;
-                    for (int hardwareWarpLoop = 0; hardwareWarpLoop < derivatives.hardwareWarpsPerSm; hardwareWarpLoop++) {
-                        if (hardwareWarpLoop == 0) {
-                            smallestNumber = dontFits[hardwareWarpLoop];
-                        } else {
-                            if (smallestNumber > dontFits[hardwareWarpLoop]) {
-                                smallestNumber = dontFits[hardwareWarpLoop];
-                            }
-                        }
-                    }
-                    for (int hardwareWarpLoop = 0; hardwareWarpLoop < derivatives.hardwareWarpsPerSm; hardwareWarpLoop++) {
-                        if (smallestNumber == dontFits[hardwareWarpLoop]) {
-                            bestHardwareWarp = hardwareWarpLoop;
-                        }
-                    }
-                    for (int laneLoop = 0; laneLoop < info.warpSize; laneLoop++) {
-                        currentTime = gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (bestHardwareWarp * info.warpSize) + laneLoop].getTypicalL1Time();
-                        gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (bestHardwareWarp * info.warpSize) + laneLoop].setTypicalL1Time(((((long double) data.time[blockLoop + laneLoop]) / ((long double) (derivatives.smallNumberOfTrialsDivisor * derivatives.smallNumberOfTrialsDivisor))) + currentTime) / 2);
-                    }
-                } else {
-                    for (int hardwareWarpLoop = 0; hardwareWarpLoop < hardwareWarpScore; hardwareWarpLoop++) {
-                        for (int laneLoop = 0; laneLoop < info.warpSize; laneLoop++) {
-                            if (std::abs(gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (hardwareWarpLoop * info.warpSize) + laneLoop].getTypicalL1Time() - (((long double) data.time[blockLoop]) / ((long double) (derivatives.smallNumberOfTrialsDivisor * derivatives.smallNumberOfTrialsDivisor)))) >= prop.maxDelta) {
-                                dontFits[hardwareWarpLoop]++;
-                            }
-                        }
-                    }
-                    smallestNumber = 0;
-                    bestHardwareWarp = 0;
-                    for (int hardwareWarpLoop = 0; hardwareWarpLoop < hardwareWarpScore; hardwareWarpLoop++) {
-                        if (hardwareWarpLoop == 0) {
-                            smallestNumber = dontFits[hardwareWarpLoop];
-                        } else {
-                            if (smallestNumber > dontFits[hardwareWarpLoop]) {
-                                smallestNumber = dontFits[hardwareWarpLoop];
-                            }
-                        }
-                    }
-                    for (int hardwareWarpLoop = 0; hardwareWarpLoop < hardwareWarpScore; hardwareWarpLoop++) {
-                        if (smallestNumber == dontFits[hardwareWarpLoop]) {
-                            bestHardwareWarp = hardwareWarpLoop;
-                        }
-                    }
-                    if (dontFits[bestHardwareWarp] > prop.maxDontFit) {
-                        for (int laneLoop = 0; laneLoop < info.warpSize; laneLoop++) {
-                            gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (hardwareWarpScore * info.warpSize) + laneLoop].setTypicalL1Time(((long double) data.time[blockLoop + laneLoop]) / ((long double) (derivatives.smallNumberOfTrialsDivisor * derivatives.smallNumberOfTrialsDivisor)));
-                        }
-                    } else {
-                        for (int laneLoop = 0; laneLoop < info.warpSize; laneLoop++) {
-                            currentTime = gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (bestHardwareWarp * info.warpSize) + laneLoop].getTypicalL1Time();
-                            gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (bestHardwareWarp * info.warpSize) + laneLoop].setTypicalL1Time(((((long double) data.time[blockLoop + laneLoop]) / ((long double) (derivatives.smallNumberOfTrialsDivisor * derivatives.smallNumberOfTrialsDivisor))) + currentTime) / 2);
-                        }
-                    }
+                for (int laneLoop = 0; laneLoop < info.warpSize; laneLoop++) {
+                    currentTime = gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (data.warp[blockLoop] * info.warpSize) + laneLoop].getTypicalL1Time();
+                    gpuCores[(data.mulp[blockLoop] * derivatives.hardwareWarpsPerSm * info.warpSize) + (data.warp[blockLoop] * info.warpSize) + laneLoop].setTypicalL1Time(((((long double) data.time[blockLoop + laneLoop]) / ((long double) (derivatives.smallNumberOfTrialsDivisor * derivatives.smallNumberOfTrialsDivisor))) + currentTime) / 2);
                 }
             }
         }
