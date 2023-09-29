e24176bfabc13c9ab501d92187f9fe03386001bd
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-29 20:37:25 +0200

    next

--- src/41-L2_Cache_Benchmark.cuh
+++ src/41-L2_Cache_Benchmark.cuh
@@ -30,6 +30,7 @@ __global__ void smallL2Benchmark(unsigned int *deviceLoad, float *deviceTime, in
 
         //Load Data in L2 cache.
         for (int j = 0; j < 1024; j++) {
+            ptr = deviceLoad + value;
             asm volatile ("ld.global.cg.u32 %0, [%1];" : "=r"(value) : "l"(ptr) : "memory");
         }
 
