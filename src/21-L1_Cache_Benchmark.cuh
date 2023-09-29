0ce90271ad445f3846e4d3b93c8f7067042ad492
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-29 20:15:41 +0200

    next

--- src/21-L1_Cache_Benchmark.cuh
+++ src/21-L1_Cache_Benchmark.cuh
@@ -31,12 +31,13 @@ __global__ void smallL1Benchmark(unsigned int *deviceLoad, float *deviceTime, in
                 unsigned int value = 0;
                 unsigned int *ptr;
 
+                /*
                 //Load data in l1 cache.
                 for (int j = 0; j < 1024; j++) {
                     ptr = deviceLoad + value;
                     asm volatile ("ld.global.ca.u32 %0, [%1];" : "=r"(value) : "l"(ptr) : "memory");
                 }
-
+*/
 
                 //Perform benchmark.
                 asm volatile("mov.u64 %0, %%globaltimer;" : "=l"(startTime));
