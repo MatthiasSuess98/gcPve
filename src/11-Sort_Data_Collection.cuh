417898482ee9050d953df20a269882921ea711b4
MatthiasSuess98 <104632950+MatthiasSuess98@users.noreply.github.com>
2023-09-30 20:09:48 +0200

    final

--- src/11-Sort_Data_Collection.cuh
+++ src/11-Sort_Data_Collection.cuh
@@ -117,7 +117,9 @@ std::vector<CoreCharacteristics> sortDataCollection(GpuInformation info, Benchma
                     gpuCores[(data.mulp[i] * derivatives.hardwareWarpsPerSm * info.warpSize) + (hardwareWarpScore * info.warpSize) + j].setTypicalGmTime(data.timeGM[i + j]);
                 }
             } else {
-                printf("%d", data.mulp[i]);
+                if (data.mulp[i] == 29) {
+                    printf("%d", data.timeL1[i]);
+                }
                 for (int j = 0; j < info.warpSize; j++) {
                     currentTime = gpuCores[(data.mulp[i] * derivatives.hardwareWarpsPerSm * info.warpSize) + (bestHardwareWarp * info.warpSize) + j].getTypicalL1Time();
                     gpuCores[(data.mulp[i] * derivatives.hardwareWarpsPerSm * info.warpSize) + (bestHardwareWarp * info.warpSize) + j].setTypicalL1Time((data.timeL1[i + j] + currentTime) / 2);
@@ -141,4 +143,3 @@ std::vector<CoreCharacteristics> sortDataCollection(GpuInformation info, Benchma
 
 //FINISHED
 
-//2929292929292929292929292929292929292929292929292929292929292929
